9b6bb7ab322f054d95e78f83bda706c389cc7106
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-03-15 03:02:07 -0700

    setting up mongo contianer

--- app.spec.ts
+++
@@ -1,7 +0,0 @@
-import {calculateDiscount} from "./src/utils";
-describe('App', () => {
-    it("should give correct value", () => {
-        const discount = calculateDiscount(100, 10);
-        expect(discount).toBe(10);
-    });
-    });
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@
 import logger from "./config/logger";
 import app from "./app";
 import config from "config";
-const startServer = () => {
+import { initDb } from "./config/db";
+const startServer = async () => {
     try {
+        await initDb()
+        logger.info("Database connected successfully");
         app.listen(config.get("server.port"), () => {
             // console.log(`Auth Service is running on port ${Config.PORT}`);
             logger.info(`catalog Service is running on port ${config.get("server.port") }`);
@@ -15,4 +18,4 @@ const startServer = () => {
     }
 };
 
-startServer();
+void startServer();
