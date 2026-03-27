be565313ad9cef3bc371a32050f425a5ffeafaad
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-03-28 02:19:35 +0500

    topping service

---
+++ src/topping/topping-router.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+import { asyncWrapper } from "../common/utils/wrapper";
+import authenticate from "../common/middlewares/authenticate";
+import { canAccess } from "../common/middlewares/canAccess";
+import { Roles } from "../common/constants";
+import { S3Storage } from "../common/services/S3Storage";
+import createHttpError from "http-errors";
+import createToppingValidator from "./create-topping-validator";
+import { ToppingService } from "./topping-service";
+import { ToppingController } from "./topping-controller";
+import { createMessageProducerBroker } from "../common/factories/brokerFactory"
+
+const router = express.Router();
+
+const toppingService = new ToppingService();
+const broker = createMessageProducerBroker();
+
+const toppingController = new ToppingController(
+    new S3Storage(),
+    toppingService,
+    broker,
+);
+
+router.post(
+    "/",
+    authenticate,
+    canAccess([Roles.ADMIN, Roles.MANAGER]),
+    fileUpload({
+        limits: { fileSize: 500 * 1024 }, // 500kb
+        abortOnLimit: true,
+        limitHandler: (req, res, next) => {
+            const error = createHttpError(400, "File size exceeds the limit");
+            next(error);
+        },
+    }),
+    createToppingValidator,
+    asyncWrapper(toppingController.create),
+);
+
+router.get("/", asyncWrapper(toppingController.get));
+
+export default router;
